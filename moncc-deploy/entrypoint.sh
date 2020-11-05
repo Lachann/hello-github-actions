40a3fec6dea5634f578ab479e31f37b71cbec75b
Luke Goral <luke.oidhche@gmail.com>
2020-11-05 17:01:01 +0100

    asdf

--- moncc-deploy/entrypoint.sh
+++ moncc-deploy/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-monccd -s /var/run/monccd.sock --cluster-only --hypervised -d &
+monccd -s /var/run/monccd.sock --cluster-only --hypervised &
 
 MNCC="mncc -s /var/run/monccd.sock"
 sleep 10
-$MNCC login --email $MONCC_USERNAME --password $MONCC_PASSWORD
+$MNCC login --email $MONCC_USER --password $MONCC_PASS
 $MNCC cluster switch -m $MONCCODE
 $MNCC load template.yaml
 $MNCC update my-namespace/my-runnable
